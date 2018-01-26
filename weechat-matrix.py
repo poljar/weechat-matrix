5efea04b64cafc847e6134e11216dd88c2165175
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-01-26 18:22:06 +0100

    Change the licence to ISC and add license headers.

--- weechat-matrix.py
+++ weechat-matrix.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
+# Weechat Matrix Protocol Script
+# Copyright © 2018 Damir Jelić <poljar@termina.org.uk>
+#
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that the
+# above copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+# CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 from __future__ import unicode_literals
 
 import json
@@ -65,7 +80,7 @@ WEECHAT_SCRIPT_NAME        = "matrix"                               # type: str
 WEECHAT_SCRIPT_DESCRIPTION = "matrix chat plugin"                   # type: str
 WEECHAT_SCRIPT_AUTHOR      = "Damir Jelić <poljar@termina.org.uk>"  # type: str
 WEECHAT_SCRIPT_VERSION     = "0.1"                                  # type: str
-WEECHAT_SCRIPT_LICENSE     = "MIT"                                  # type: str
+WEECHAT_SCRIPT_LICENSE     = "ISC"                                  # type: str
 
 
 class MatrixUser:
