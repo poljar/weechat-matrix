20bf4265332e59372bbad8b54b54584217f292c9
Denis Kasak <dkasak@termina.org.uk>
2019-02-07 23:15:25 +0100

    Fix encoding errors with non-ascii chars.

--- matrix/markdown_parser.py
+++ matrix/markdown_parser.py
@@ -16,6 +16,8 @@
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+from __future__ import unicode_literals
+
 import html
 import textwrap
 
