1fe0215e4472295968e2c03d183505bdb923a1a3
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-11-11 14:40:18 +0200

    Some Telegram bridges use "_telegram" prefix instead of plain "telegram"

--- matrix/buffer.py
+++ matrix/buffer.py
@@ -1057,6 +1057,7 @@ class RoomBuffer(object):
                 user.user_id.startswith("@whatsapp_") or
                 user.user_id.startswith("@facebook_") or
                 user.user_id.startswith("@telegram_") or
+                user.user_id.startswith("@_telegram_") or
                 user.user_id.startswith("@_xmpp_")):
             if user.display_name:
                 short_name = user.display_name[0:50]
